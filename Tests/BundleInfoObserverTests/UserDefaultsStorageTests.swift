f048e20a969d5648d3de5f96d84e7f5ad27d1e27
Alex Cristea <alexcristea@me.com>
2019-10-18 21:46:11 +0300

    Refactor UserDefaultsStorageTests

--- Tests/BundleInfoObserverTests/UserDefaultsStorageTests.swift
+++ Tests/BundleInfoObserverTests/UserDefaultsStorageTests.swift
@@ -1,7 +1,6 @@
 import XCTest
 @testable import BundleInfoObserver
 
-
 final class UserDefaultsStorageTests: XCTestCase {
     
     func test_saveValue() {
@@ -17,47 +16,80 @@ final class UserDefaultsStorageTests: XCTestCase {
         XCTAssertEqual(userDefaults.value, "value")
     }
     
-    func test_key_whenSavingValue() {
-        
-        //arrange
-        let userDefaults = FakeUserDefaults<String>()
-        let sut = UserDefaultsStorage(userDefaults: userDefaults)
-        
-        //act
-        sut.set(value: "value", for: "key")
-        
-        //assert
-        XCTAssertEqual(userDefaults.composedKey, "com.nsagora.storage.key")
-    }
-    
     func test_getValue() {
 
         //arrange
         let userDefaults = FakeUserDefaults<String>()
-        
         let sut = UserDefaultsStorage(userDefaults: userDefaults)
         sut.set(value: "value", for: "key")
         
         //act
-        let expected: String? = sut.getValue(for: "key")
-        
+        let expected = sut.getValue(for: "key") as String?
         
         //assert
         XCTAssertEqual(expected, "value")
     }
     
-    func test_key_whenGettingValue() {
+    func test_keyComposition_whenSavingValue() {
         
         //arrange
         let userDefaults = FakeUserDefaults<String>()
-        
         let sut = UserDefaultsStorage(userDefaults: userDefaults)
-        sut.set(value: "value", for: "key")
         
         //act
-        let _: String? = sut.getValue(for: "key")
+        sut.set(value: "value", for: "key")
         
         //assert
         XCTAssertEqual(userDefaults.composedKey, "com.nsagora.storage.key")
     }
+    
+    func test_keyPathComposition_whenSavingValue() {
+        
+        //arrange
+        let userDefaults = FakeUserDefaults<String>()
+        let sut = UserDefaultsStorage(userDefaults: userDefaults)
+        
+        //act
+        sut.set(value: "value", for: "key/path")
+        
+        //assert
+        XCTAssertEqual(userDefaults.composedKey, "com.nsagora.storage.key-path")
+    }
+
+    func test_keyComposition_whenGettingValue() {
+        
+        //arrange
+        let userDefaults = FakeUserDefaults<String>()
+        let sut = UserDefaultsStorage(userDefaults: userDefaults)
+        sut.set(value: "value", for: "key")
+        
+        //act
+        let _  = sut.getValue(for: "key") as String?
+        
+        //assert
+        XCTAssertEqual(userDefaults.composedKey, "com.nsagora.storage.key")
+    }
+    
+    func test_keyPathComposition_whenGettingValue() {
+        
+        //arrange
+        let userDefaults = FakeUserDefaults<String>()
+        let sut = UserDefaultsStorage(userDefaults: userDefaults)
+        sut.set(value: "value", for: "key/path")
+        
+        //act
+        let _  = sut.getValue(for: "key/path") as String?
+        
+        //assert
+        XCTAssertEqual(userDefaults.composedKey, "com.nsagora.storage.key-path")
+    }
+    
+    static var allTests = [
+        ("test_saveValue", test_saveValue),
+        ("test_keyComposition_whenSavingValue", test_keyComposition_whenSavingValue),
+        ("test_keyPathComposition_whenSavingValue", test_keyPathComposition_whenSavingValue),
+        ("test_getValue", test_getValue),
+        ("test_keyComposition_whenGettingValue", test_keyComposition_whenGettingValue),
+        ("test_keyPathComposition_whenGettingValue", test_keyPathComposition_whenGettingValue),
+    ]
 }
--- Tests/BundleInfoObserverTests/XCTestManifests.swift
+++ Tests/BundleInfoObserverTests/XCTestManifests.swift
@@ -3,7 +3,8 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(AppInfoObserverTests.allTests),
+        testCase(BundleInfoObserverTests.allTests),
+        testCase(UserDefaultsStorageTests.allTests),
     ]
 }
 #endif
