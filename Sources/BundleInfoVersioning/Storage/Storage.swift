aa4a337912f5e3873654c84cc3e6dadf1c8f6992
Alex Cristea <alexcristea@me.com>
2020-02-21 14:38:03 +0200

    Update documentation

--- Sources/BundleInfoVersioning/Storage/Storage.swift
+++ Sources/BundleInfoVersioning/Storage/Storage.swift
@@ -1,10 +1,23 @@
 import Foundation
 
 /**
-    
+ The `Storage` protocol is used to define the structre for custom key-value storage implementations.
  */
 public protocol Storage {
     
+    /**
+     Stores a generic `T` value for a given key.
+     
+     - parameter value: generic `T` value to store.
+     - parameter key: the key associated with the value.
+     */
     func set<T>(value: T?, for key: String)
+
+    /**
+     Retrieves  generic `T` value for a given key.
+     
+     - parameter key: the key associated with the value.
+     - returns: generic `T` value.
+     */
     func getValue<T>(for key: String) -> T?
 }
